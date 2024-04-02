997f5b624696ded12e4bce558fe7a81996a56149
MilagrosPozzo <165667216+MilagrosPozzo@users.noreply.github.com>
2024-04-02 20:41:16 -0300

    Add files via upload

---
+++ Suma 2 números.py
@@ -0,0 +1,7 @@
+# Definir dos variables con números específicos asignados por ti
+n1 = int(input("Número 1: "))
+n2 = int(input("Número 2: "))
+# Sumar los dos números y almacenar el resultado en una tercera variable
+x = n1 + n2
+# Mostrar el resultado de la suma
+print("La suma ",x)
